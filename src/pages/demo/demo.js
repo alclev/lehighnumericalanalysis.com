c8cf38551627dfa3ef74afae82dee15bc81629e2
Alex Clevenger <alexbclevenger@gmail.com>
2023-04-05 14:26:00 -0400

    Deploy 2023-04-05 14:25:48

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -10,6 +10,7 @@ function Demo() {
         // Call the add function exported from the module
         const result = module.instance.exports.add(2, 2);
         setResult(`The result of 2 + 2 is ${result}.`);
+        console.log(`The result of 2 + 2 is ${result}.`)
       });
   }, []);
 
