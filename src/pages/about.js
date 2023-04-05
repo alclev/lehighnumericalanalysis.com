de06c7a77ee33fcba9cf25bf57757571748d4991
Alex Clevenger <alexbclevenger@gmail.com>
2023-04-05 02:17:15 -0400

    local development

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './about.css';
 
 function About() {
   return (
     <div>
-      <p>This is some about page content.</p>
+      <p>We are a team of Lehigh undergraduates developing and testing low-level programming interfaces to operation on numerical systems.</p>
     </div>
   );
 }
