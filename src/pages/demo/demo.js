cd327b75ec81b43b25e9333560bf7c4f164678b8
Alex Clevenger <alexbclevenger@gmail.com>
2023-04-06 13:55:10 -0400

    local development 2023-04-06 13:55:10

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -5,7 +5,7 @@ function Demo() {
     return WebAssembly.compile(bytes).then(m => new WebAssembly.Instance(m, imports));
   }
   var importObject = { imports: { i: arg => console.log(arg) } };  
-  fetch('add.wasm').then(response => response.arrayBuffer())
+  fetch('./add.wasm').then(response => response.arrayBuffer())
   .then(bytes => instantiate(bytes, importObject))
   .then(instance => instance.exports.e());
   return (
