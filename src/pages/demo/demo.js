8c8476f01fb60051d7a6ac05792f4c316151ca96
Alex Clevenger <alexbclevenger@gmail.com>
2023-04-11 14:06:30 -0400

    local development 2023-04-11 14:06:30

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
 function Demo() {
-  function instantiate(bytes, imports) {
-    return WebAssembly.compile(bytes).then(m => new WebAssembly.Instance(m, imports));
-  }
-  var importObject = { imports: { i: arg => console.log(arg) } };  
-  fetch('./add.wasm').then(response => response.arrayBuffer())
-  .then(bytes => instantiate(bytes, importObject))
-  .then(instance => instance.exports.e());
-  return (
-    <div>
-      <p>First trial of embedding webassembly</p>
-      <p>Test</p>
-    </div>
-  );
+  fetch('./add.js')
+  .then(response => response.arrayBuffer())
+  .then(buffer => WebAssembly.instantiate(buffer))
+  .then(instance => {
+    const add = instance.exports._add; // Get the exported "add" function
+    const result = add(3, 5); // Call the "add" function with two arguments
+    console.log(result); // Output: 8
+  });
 }
 
 export default Demo;
