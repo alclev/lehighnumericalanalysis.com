481406741605115912e52f2cc2717c2d502718f1
Alex Clevenger <alexbclevenger@gmail.com>
2023-04-11 14:45:19 -0400

    local development 2023-04-11 14:45:19

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -1,8 +1,18 @@
 
 
 function Demo() {
-  add = Module.cwrap('add', 'number', ['number', 'number']);
-  console.log(add(1, 2));
+  // Load the add.js module
+  fetch('add.js')
+  .then(response => response.arrayBuffer())
+  .then(buffer => WebAssembly.instantiate(buffer))
+  .then(module => {
+    // Get a reference to the _add function
+    const add = module.instance.exports._add;
+
+    // Call the _add function and print the result
+    const result = add(2, 3);
+    console.log(result); // Output: 5
+  });
 
 }
 
