4918a661951d9986c7e74b35d0ac35885820a4f6
Alex Clevenger <alexbclevenger@gmail.com>
2023-04-11 14:51:10 -0400

    local development 2023-04-11 14:51:10

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -1,17 +1,16 @@
 
-
 function Demo() {
   // Load the add.js module
   fetch('add.js')
-  .then(response => response.arrayBuffer())
-  .then(buffer => WebAssembly.instantiate(buffer))
-  .then(module => {
-    // Get a reference to the _add function
-    const add = module.instance.exports._add;
+    .then(response => response.arrayBuffer())
+    .then(buffer => WebAssembly.instantiate(buffer))
+    .then(module => {
+      // Get a reference to the _add function
+      const add = module.instance.exports.add;
 
-    // Call the _add function and print the result
-    const result = add(2, 3);
-    console.log(result); // Output: 5
+      // Call the _add function and print the result
+      const result = add(2, 3);
+      console.log(result); // Output: 5
   });
 
 }
