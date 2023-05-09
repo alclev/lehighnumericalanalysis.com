9ea83650d696621f6db83339638e2b1ba1affc07
Alex Clevenger <alexbclevenger@gmail.com>
2023-05-09 16:36:34 -0400

    Deploy 2023-05-09 16:36:23

--- matrix_generator.py
+++ matrix_generator.py
@@ -1,18 +1,17 @@
 import os
 
-filename = 'valid_matrix2.data'
+base_dir = '~/Downloads/'
+filename = 'valid_matrix2.csv'
+output = os.path.join(base_dir, filename)
 num_rows = 25
 num_cols = 25
 
 def generate_matrix(file):
     file.write(str(num_rows) + ', ' + str(num_cols) + '\n')
-    for(i in range(num_rows * num_cols)):
-        file.write(str(i) + ', ')
+    for i in range(num_rows * num_cols):
+        file.write(str(i))
+        if(i < num_rows * num_cols - 1):
+            file.write(', ')
+    file.close()
 
-if os.path.isfile(filename):
-    with open(filename, 'w') as f:
-        generate_matrix(f)
-else:
-    with open(filename, 'x') as f:
-        generate_matrix(f)
-
+generate_matrix(open(filename, "w"))
