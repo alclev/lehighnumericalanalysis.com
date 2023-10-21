8b7ce4e02c3c4cb9f00915b3c5753052113fec11
Alex Clevenger <alexbclevenger@gmail.com>
2023-10-21 17:18:08 -0400

    Deploy 2023-10-21 17:17:53

--- src/api.js
+++ src/api.js
@@ -17,8 +17,7 @@ export const handleAddition = function (matrixA, matrixB) {
     .then(function (response) {
       console.log(response);
       console.log(response.data.data);
-      const result = JSON.stringify(response.data.data);
-      return result.data;
+      return JSON.stringify(response.data.data);
     })
     .catch((error) => {
       console.error('Error making POST request:', error);
